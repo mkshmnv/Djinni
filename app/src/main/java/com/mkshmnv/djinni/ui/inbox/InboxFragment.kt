2e2b3c631b9699bccd99ffc1861e249fe33539ce
Mykola <nickkosh@gmail.com>
2023-11-26 10:06:53 +0200

    save

--- app/src/main/java/com/mkshmnv/djinni/ui/inbox/InboxFragment.kt
+++ app/src/main/java/com/mkshmnv/djinni/ui/inbox/InboxFragment.kt
@@ -1,19 +1,55 @@
 package com.mkshmnv.djinni.ui.inbox
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.mkshmnv.djinni.Logger
 import com.mkshmnv.djinni.R
 import com.mkshmnv.djinni.databinding.FragmentInboxBinding
 import com.mkshmnv.djinni.ui.viewBinding
 
 class InboxFragment : Fragment(R.layout.fragment_inbox) {
-    private val binding: FragmentInboxBinding by viewBinding()
+    private lateinit var binding: FragmentInboxBinding// by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Logger.logcat("onViewCreated", this::class.simpleName)
+        binding = FragmentInboxBinding.bind(view)
+        // Ініціалізація TabLayout
+        binding.tabLayout.apply {
+            addTab(newTab().setText(getString(R.string.inbox_filter_all)))
+            addTab(newTab().setText(getString(R.string.inbox_filter_new)))
+            addTab(newTab().setText(getString(R.string.inbox_filter_active)))
+            addTab(newTab().setText(getString(R.string.inbox_filter_favorites)))
+            addTab(newTab().setText(getString(R.string.inbox_filter_archive)))
+            addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    // Код, який викликається при виборі таби
+                    tab?.let {
+                        val selectedTabIndex = it.position
+                        Logger.logcat("Clicked $selectedTabIndex - ${tab.text}")
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                    // Код, який викликається при відміні вибору таби
+                    tab?.let {
+                        // Робіть щось із табою, яка була вибрана, але тепер не вибрана
+                    }
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                    // Код, який викликається при повторному виборі вже вибраної таби
+                    tab?.let {
+                        // Робіть щось при повторному виборі таби
+                    }
+                }
+            })
+        }
 
     }
 }
--- app/src/main/java/com/mkshmnv/djinni/ui/profile/PagerProfileFragment.kt
+++ app/src/main/java/com/mkshmnv/djinni/ui/profile/PagerProfileFragment.kt
@@ -6,11 +6,11 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mkshmnv.djinni.Logger
 import com.mkshmnv.djinni.R
-import com.mkshmnv.djinni.databinding.FragmentPagerProfileBinding
+import com.mkshmnv.djinni.databinding.FragmentPagerBinding
 import com.mkshmnv.djinni.ui.viewBinding
 
-class PagerProfileFragment : Fragment(R.layout.fragment_pager_profile) {
-    private val binding: FragmentPagerProfileBinding by viewBinding()
+class PagerProfileFragment : Fragment(R.layout.fragment_pager) {
+    private val binding: FragmentPagerBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,7 +19,7 @@ class PagerProfileFragment : Fragment(R.layout.fragment_pager_profile) {
         binding.apply {
             viewpager.adapter = PagerAdapter(requireActivity())
             TabLayoutMediator(tabs, viewpager) { tab, position ->
-                tab.text = when(position) {
+                tab.text = when (position) {
                     0 -> getString(R.string.tab_profile)
                     1 -> getString(R.string.tab_cv)
                     2 -> getString(R.string.tab_subs)
